c8b916f6ba755c33e12aa5270e73d4c9047a11d7
VishalChudasama08 <chudasamavishal183@gmail.com>
2023-08-23 12:11:04 +0530

    oracle all update tile time

---
+++ Sem-3/Oracle/leb_assignment/1_academy_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE academy(st_no NUMBER(3), class NUMBER(2), name VARCHAR2(15), game1 VARCHAR2(25), g1_grade VARCHAR2(2), game2 VARCHAR2(25), g2_grade VARCHAR2(2))
+/
+INSERT INTO academy(st_no, class, name, game1, g1_grade, game2, g2_grade) VALUES (10, 7, 'Samir', 'Cricket', 'B', 'Swimming', 'A')
+/
+INSERT INTO academy(st_no, class, name, game1, g1_grade, game2, g2_grade) VALUES (11, 8, 'Sujit', 'Tennis', 'A', 'Skating', 'C')
+/
+INSERT INTO academy(st_no, class, name, game1, g1_grade, game2, g2_grade) VALUES (12, 7, 'Lara', 'Swimming', 'B', 'Football', 'B')
+/
+INSERT INTO academy(st_no, class, name, game1, g1_grade, game2, g2_grade) VALUES (13, 7, 'Veena', 'Tennis', 'C', 'Tennis', 'A')
+/
+INSERT INTO academy(st_no, class, name, game1, g1_grade, game2, g2_grade) VALUES (14, 9, 'Archana', 'Basket ball', 'A', 'Cricket', 'A')
+/
+INSERT INTO academy(st_no, class, name, game1, g1_grade, game2, g2_grade) VALUES (15, 10, 'Arpit', 'Cricket', 'A', 'Athletics', 'C')
+/
