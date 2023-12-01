1678cd3caabe9ad19863d4fba2f595322e1a0233
VishalChudasama08 <chudasamavishal183@gmail.com>
2023-12-01 15:46:42 +0530

    add c# visual studio projects

---
+++ Sem-4/C#/HW_001/HW_001/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HW_001 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
