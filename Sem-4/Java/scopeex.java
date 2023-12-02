bd5b5c35bb8730c614c39215fa37e736f41193ad
VishalChudasama08 <chudasamavishal183@gmail.com>
2023-12-02 18:29:24 +0530

    fix error

--- Sem-4/Java/scopeex.java
+++ Sem-4/Java/scopeex.java
@@ -16,6 +16,7 @@ class scopeex {
 
     static void scope() {
         int c = 30;
+        System.out.println(c); // not print becose not colling this method
         // System.out.println("a is " + a); // error : a scope is not here....
     }
 }
