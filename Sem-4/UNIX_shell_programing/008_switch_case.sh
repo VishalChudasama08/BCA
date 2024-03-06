6bd370df923a8a5273febe27acf92059906c2449
VishalChudasama08 <chudasamavishal183@gmail.com>
2024-03-06 22:10:51 +0530

    008

--- Sem-4/UNIX_shell_programing/008_switch_case.sh
+++ Sem-4/UNIX_shell_programing/008_switch_case.sh
@@ -1,11 +1,36 @@
 clear
-if $y -le 1999 -a $m -le 13
-then 
-    echo "Invalid Your ar month";
+if "$y" -le 1999 -a "$m" -le 13
+then
+    echo "Invalid year or month"
 else
-    cal $y $m
+    cal "$y" "$m"
 fi
-case $m in
-    1) echo "february"
-    cal $m;;
-    2) echos
+
+case "$m" in
+    1) echo "January"
+       ;;
+    2) echo "February"
+       ;;
+    3) echo "March"
+       ;;
+    4) echo "April"
+       ;;
+    5) echo "May"
+       ;;
+    6) echo "June"
+       ;;
+    7) echo "July"
+       ;;
+    8) echo "August"
+       ;;
+    9) echo "September"
+       ;;
+    10) echo "October"
+        ;;
+    11) echo "November"
+        ;;
+    12) echo "December"
+        ;;
+    *) echo "Invalid month"
+       ;;
+esac
